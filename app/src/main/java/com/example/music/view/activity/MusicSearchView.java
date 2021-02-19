9126aff8522fd9f1ef9f1de7217cea45d2bbb315
zz010625 <1140143252@qq.com>
2021-02-19 12:14:08 +0800

    接口 用于MusicSearchActivity

--- app/src/main/java/com/example/music/view/activity/MusicSearchView.java
+++ app/src/main/java/com/example/music/view/activity/MusicSearchView.java
@@ -3,6 +3,6 @@ package com.example.music.view.activity;
 import java.util.ArrayList;
 
 public interface MusicSearchView {
-    void initRecyclerViewAdapt(ArrayList musicArrayList,ArrayList playerMusicArrayList);
+    void initRecyclerViewAdapt(ArrayList musicArrayList, ArrayList playerMusicArrayList);
 
 }
